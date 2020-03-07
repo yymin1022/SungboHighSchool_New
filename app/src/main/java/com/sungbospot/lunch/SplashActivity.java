acac0fd48064af1f00a57ae0cc489354029f57e7
yymin1022 <yymin1022@gmail.com>
2020-03-07 22:50:25 +0900

    Update SplashActivity

--- app/src/main/java/com/sungbospot/lunch/SplashActivity.java
+++ app/src/main/java/com/sungbospot/lunch/SplashActivity.java
@@ -3,6 +3,7 @@ package com.sungbospot.lunch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 1500);
     }
 }
