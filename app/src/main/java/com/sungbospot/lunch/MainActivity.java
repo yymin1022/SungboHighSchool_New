9cb8afeb361b01fc46dcbda0ebc256dd1262b842
yymin1022 <yymin1022@gmail.com>
2020-03-08 15:16:07 +0900

    Update MainActivity

--- app/src/main/java/com/sungbospot/lunch/MainActivity.java
+++ app/src/main/java/com/sungbospot/lunch/MainActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnDevinfo;
+    Button btnMeal;
+    Button btnNotice;
+    Button btnSchoolinfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startActivity(new Intent(this, SplashActivity.class));
+
+        btnDevinfo = findViewById(R.id.main_btn_devInfo);
+        btnMeal = findViewById(R.id.main_btn_meal);
+        btnNotice = findViewById(R.id.main_btn_notice);
+        btnSchoolinfo = findViewById(R.id.main_btn_schoolInfo);
+
+        View.OnClickListener btnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.main_btn_devInfo:
+                        startActivity(new Intent(getApplicationContext(), DevinfoActivity.class));
+                        break;
+                    case R.id.main_btn_meal:
+                        startActivity(new Intent(getApplicationContext(), MealActivity.class));
+                        break;
+                    case R.id.main_btn_notice:
+                        startActivity(new Intent(getApplicationContext(), NoticeActivity.class));
+                        break;
+                    case R.id.main_btn_schoolInfo:
+                        startActivity(new Intent(getApplicationContext(), SchoolinfoActivity.class));
+                        break;
+                }
+            }
+        };
+
+        btnDevinfo.setOnClickListener(btnListener);
+        btnMeal.setOnClickListener(btnListener);
+        btnNotice.setOnClickListener(btnListener);
+        btnSchoolinfo.setOnClickListener(btnListener);
     }
 }
