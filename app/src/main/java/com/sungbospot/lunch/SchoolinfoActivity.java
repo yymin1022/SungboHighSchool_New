ec3e732e4afb30f44d7f3af6d239b79b0d09c5cd
yymin1022 <yymin1022@gmail.com>
2020-03-08 14:29:24 +0900

    Add Activities

---
+++ app/src/main/java/com/sungbospot/lunch/SchoolinfoActivity.java
@@ -0,0 +1,14 @@
+package com.sungbospot.lunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SchoolinfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schoolinfo);
+    }
+}
